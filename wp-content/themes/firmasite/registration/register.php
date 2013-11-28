d251fb06dd42746cc88a3bfba35f44fd3da9dcbd
Anton Shumeika <shumeikaanton@gmail.com>
2013-11-28 07:05:06 +0100

    changing after registration message

--- wp-content/themes/firmasite/registration/register.php
+++ wp-content/themes/firmasite/registration/register.php
@@ -301,7 +301,7 @@ get_header( 'buddypress' ); ?>
 				<?php do_action( 'bp_before_registration_confirmed' ); ?>
 
 				<?php if ( bp_registration_needs_activation() ) : ?>
-					<p><?php _e( 'You have successfully created your account! To begin using this site you will need to activate your account via the email we have just sent to your address.', "firmasite" ); ?></p>
+					<p><?php _e( 'You have successfully registered for the IT Mentorship! To begin using this site you will need to activate your account via the email we have just sent to your address.', "firmasite" ); ?></p>
 				<?php else : ?>
 					<p><?php _e( 'You have successfully created your account! Please log in using the username and password you have just created.', "firmasite" ); ?></p>
 				<?php endif; ?>
